7a6d1d4f8b9aa5bbc094c92e337b0d17a95c762b
Evan hameed <evanhameed9966@gmail.com>
2020-05-12 11:01:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+const names=["evan", "nashwan", "Tony"];
+
+function writeCards(names,eventName){
+
+for (let i =0 ; i <= names.length-1;i++){
+
+  console.log(`Thank you ${names[i]} for the ${eventName} gift );
+}
+
+}
