e602eda03b596eb8ab989756b70c10a3fdd5f4a2
Evan hameed <evanhameed9966@gmail.com>
2020-05-12 11:40:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 function writeCards(names,eventName){
 for( let i=0;i<=names.length-1;i++){
 
-  names[i] = [ ` Thank you ${names[i]} for the ${eventName} gift`];
+  names[i] = [ `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`];
 }
 return names;
 }
-writeCards(["Ada","Brendan","Ali"],"surprise");
+writeCards(["Lisa","Kaitlin","Jan"],"surprise");
