57481892adbe563553c7fac95a5bd930d2db94d2
Evan hameed <evanhameed9966@gmail.com>
2020-05-12 11:27:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solutions in this file
 
-const names=["evan", "nashwan", "Tony"];
+
 
 function writeCards(names,eventName){
-
 for( let i=0;i<=names.length-1;i++){
 
-  console.log( ` Thank you ${names[i]} for the ${eventName} gift` );
+ console.log( ` Thank you ${names[i]} for the ${eventName} gift`);
 }
+
 }
-writeCards(["evan","hameed"],"surprise");
+writeCards(["Ada","Brendan","Ali"],"surprise");
